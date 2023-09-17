b9c495a139121ba66dab53b3a04d0fe3ff0a7dd1
Arjun Sharma <120898184+arjunsharma999@users.noreply.github.com>
2023-09-17 15:57:13 +0530

    Contact-us page

--- app/pages/about/page.jsx
+++ app/pages/about/page.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-function about() {
+function page() {
   return (
-    <div>about</div>
+    <>
+
+    <section className='flex'>
+
+        <div className=' col mt-[6rem ]  lg:w-[80%] mx-auto flex flex-wrap items-center justify-between '>
+
+            
+          <h1>Who we are</h1>
+          <h2> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam omnis porro cupiditate sapiente dolorem, odit, aperiam labore veniam ut neque, magni asperiores fuga qui. Laborum facere inventore excepturi tempora dolores ab consectetur suscipit non, consequatur voluptatem ex, voluptatibus iusto reprehenderit, eos temporibus repellat? Blanditiis, velit!</h2>
+
+        </div>
+
+        <div className='mt-[6rem]   mt-[5rem] lg:w-[90%] mx-auto flex flex-wrap items-center justify-between '>
+
+            <img src="/contact/about us 1.png" alt="" />
+        </div>
+    </section>
+    
+    
+    
+    
+    </>
   )
 }
 
-export default about
+export default page
